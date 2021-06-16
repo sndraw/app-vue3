b696bf44226ab957c59f697ac41e30fedf761c87
shaoning <ningshao@ichint.com>
2021-06-16 13:36:55 +0800

    upd:新增环境变量

--- src/views/WebGL.vue
+++ src/views/WebGL.vue
@@ -1,114 +1,230 @@
 <template>
-  <div ref="container" class="webgl-container"></div>
+  <canvas ref="particle" class="webgl-container"></canvas>
+  <canvas ref="container" class="webgl-container"></canvas>
 </template>
 <script>
-import { onMounted, inject, ref } from "vue";
+import { onMounted, ref } from "vue";
+// import particeAnim from "./../utils/particleAnim";
+
 export default {
   name: "WebGL",
   setup() {
+    const particle = ref("partice-Panel");
     const container = ref("WebGL-Panel");
-    let renderer = null;
-    let camera = null;
-    let scene = null;
-    let sphere = null;
-    let cube = null;
-    let step = 1;
-    const THREE = inject("THREE");
-    const addAxes = () => {
-      //添加坐标轴
-      let axes = new THREE.AxesHelper(20);
-      scene.add(axes);
-      //平面
-      let planeGeometry = new THREE.PlaneGeometry(60, 20);
-      let planeMaterial = new THREE.MeshLambertMaterial({ color: 0xcccccc });
-      let plane = new THREE.Mesh(planeGeometry, planeMaterial);
-      plane.receiveShadow = true;
-      plane.rotation.x = -0.5 * Math.PI;
-      scene.add(plane);
-    };
-    const addLights = () => {
-      // 添加点光源
-      let spotLight = new THREE.SpotLight(0xffffff);
-      spotLight.position.set(40, 80, -20);
-      spotLight.castShadow = true;
-      spotLight.shadow.mapSize.height = 2048;
-      spotLight.shadow.mapSize.width = 2048;
-      scene.add(spotLight);
-    };
-    const addCutes = () => {
-      //方体
-      let cubeGeometry = new THREE.BoxGeometry(5, 5, 5);
-      let cubeMaterial = new THREE.MeshLambertMaterial({ color: 0xffee6b });
-      cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-      cube.castShadow = true;
-      cube.position.x = -10;
-      cube.position.y = 10;
-      scene.add(cube);
+    // 绘制背景
+    function drawBg(options) {
+      const canvas = container.value;
+      const context = canvas.getContext("2d");
+      const { bgColor } = options;
+      context.save();
+      context.globalAlpha = 1;
+      context.setTransform(1, 0, 0, 1, 0, 0);
+      context.fillStyle = bgColor;
+      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+      context.restore();
+    }
+    // 绘制网格
+    function drawGrid(options) {
+      const canvas = container.value;
+      const context = canvas.getContext("2d");
+      const {
+        bgColor,
+        lineWidth,
+        color,
+        xNum,
+        yNum,
+        position = {
+          direction: "top",
+          offsetX: 0,
+          offsetY: 0,
+        },
+      } = options;
+      const { direction } = position;
+      context.save();
+      const stepx = context.canvas.width / xNum;
+      const stepy = context.canvas.height / yNum / 2;
+      if (direction === "top") {
+        context.lineWidth = lineWidth;
+        const gradient = context.createLinearGradient(
+          context.canvas.width / 2,
+          context.canvas.height / 2,
+          context.canvas.width / 2,
+          0
+        );
+        gradient.addColorStop("0", bgColor);
+        gradient.addColorStop("1.0", color);
+        context.strokeStyle = gradient;
+        for (let i = 0, num = 1; num <= xNum + 1; i += stepx, num++) {
+          context.globalAlpha = 0.2;
+          context.setTransform(1, 0, 0, 1, 0, 0);
+          context.strokeStyle = gradient;
+          context.beginPath();
+          context.moveTo(context.canvas.width / 2, context.canvas.height / 2);
+          context.lineTo(i, 0);
+          context.closePath();
+          context.stroke();
+        }
+        for (
+          let j = 0, num = 0;
+          num <= yNum;
+          j = stepy * num * (num / yNum), num++
+        ) {
+          context.globalAlpha = 0.2;
+          context.setTransform(1, 0, 0, 1, 0, 0);
+          context.beginPath();
+          const tan = context.canvas.width / context.canvas.height;
+          context.moveTo(
+            context.canvas.width - (context.canvas.width / 2 - tan * j),
+            context.canvas.height / 2 - j
+          );
+          context.lineTo(
+            context.canvas.width - (context.canvas.width / 2 + tan * j),
+            context.canvas.height / 2 - j
+          );
+          context.closePath();
+          context.stroke();
+        }
+      }
 
-      //球体
-      let sphereGeometry = new THREE.SphereGeometry(2, 20, 20);
-      let sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
-      sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-      sphere.castShadow = true;
-      sphere.position.x = 20;
-      sphere.position.y = 5;
-      sphere.position.z = 0;
-      scene.add(sphere);
-    };
-    const animate = () => {
-      const animateHandle = () => {
-        requestAnimationFrame(animateHandle);
-        //旋转方体
-        cube.rotation.x += 0.02;
-        cube.rotation.y += 0.02;
+      if (direction === "bottom") {
+        context.lineWidth = lineWidth;
+        const gradient = context.createLinearGradient(
+          context.canvas.width / 2,
+          context.canvas.height / 2,
+          context.canvas.width / 2,
+          context.canvas.height
+        );
+        gradient.addColorStop("0", bgColor);
+        gradient.addColorStop("1.0", color);
+        context.strokeStyle = gradient;
+        for (let i = 0, num = 1; num <= xNum + 1; i += stepx, num++) {
+          context.globalAlpha = 0.2;
+          context.setTransform(1, 0, 0, 1, 0, 0);
+          context.strokeStyle = gradient;
+          context.beginPath();
+          context.moveTo(context.canvas.width / 2, context.canvas.height / 2);
+          context.lineTo(i, context.canvas.height);
+          context.closePath();
+          context.stroke();
+        }
+        for (
+          let j = 0, num = 1;
+          num <= yNum;
+          j = stepy * num * (num / yNum), num++
+        ) {
+          context.globalAlpha = 0.2;
+          context.setTransform(1, 0, 0, 1, 0, 0);
+          context.beginPath();
+          const tan = context.canvas.width / context.canvas.height;
+          context.moveTo(
+            context.canvas.width / 2 - tan * j,
+            j + context.canvas.height / 2
+          );
+          context.lineTo(
+            context.canvas.width / 2 + tan * j,
+            j + context.canvas.height / 2
+          );
+          context.closePath();
+          context.stroke();
+        }
+      }
 
-        //弹跳球
-        step += 0.03;
-        sphere.position.x = 10 + 10 * Math.cos(step);
-        sphere.position.y = 2 + 10 * Math.abs(Math.sin(step));
-        renderer.render(scene, camera);
-      };
-      animateHandle();
+      context.restore();
+    }
+    // // 移动网格
+    // function moveGrid() {}
+    // 动画开启控制
+    const animate = true;
+    let step = 0;
+    const maxStep = 100;
+    //  背景配置
+    const bgOps = {
+      bgColor: "#000",
     };
+    // 网格配置
+    const gridOps1 = {
+      bgColor: "#000",
+      color: "#fff",
+      lineWidth: 1,
+      xNum: 12,
+      yNum: 12,
+      position: {
+        direction: "top",
+        offsetX: 0,
+        offsetY: 0,
+      },
+    };
+    const gridOps2 = {
+      bgColor: "#000",
+      color: "#fff",
+      lineWidth: 1,
+      xNum: 12,
+      yNum: 12,
+      position: {
+        direction: "bottom",
+        offsetX: 0,
+        offsetY: 0,
+      },
+    };
+    function onWindowResize() {
+      const canvas = container.value;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      drawBg(bgOps);
+      drawGrid(gridOps1);
+      drawGrid(gridOps2);
+    }
     const init = () => {
-      const containerEl = container.value;
-      scene = new THREE.Scene();
-      camera = new THREE.PerspectiveCamera(
-        45,
-        containerEl.clientWidth / containerEl.clientHeight,
-        0.1,
-        1000
-      );
-      camera.position.x = 40;
-      camera.position.y = 40;
-      camera.position.z = 40;
-      camera.lookAt(scene.position);
+      const canvas = container.value;
+      const context = canvas.getContext("2d");
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
 
-      renderer = new THREE.WebGLRenderer();
-      renderer.setClearColor(0xeeeeee); //背景色
-      renderer.setSize(containerEl.clientWidth, containerEl.clientHeight); //场景大小
-      renderer.shadowMap.enabled = true; //启用阴影
-      renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-      containerEl.appendChild(renderer.domElement);
-      addAxes();
-      addLights();
-      addCutes();
-      renderer.render(scene, camera);
-      animate();
+      function animation() {
+        if (step > maxStep) {
+          step = 0;
+          gridOps1.position.offsetX = 0;
+          gridOps1.position.offsetY = 0;
+          gridOps2.position.offsetX = 0;
+          gridOps2.position.offsetY = 0;
+        } else {
+          step++;
+          const offsetX1 = 0;
+          const offsetY1 = context.canvas.height / 2 / gridOps1.yNum / maxStep;
+          const offsetX2 = 0;
+          const offsetY2 = context.canvas.height / 2 / gridOps2.yNum / maxStep;
+          gridOps1.position.offsetX += offsetX1;
+          gridOps1.position.offsetY += offsetY1;
+          gridOps2.position.offsetX += offsetX2;
+          gridOps2.position.offsetY += offsetY2;
+        }
+        drawBg(bgOps);
+        drawGrid(gridOps1);
+        drawGrid(gridOps2);
+        if (animate) {
+          window.requestAnimationFrame(animation);
+        }
+      }
+      animation();
+      window.addEventListener("resize", onWindowResize, false);
     };
+
     onMounted(() => {
       init();
     });
     return {
       container,
+      particle,
     };
   },
 };
 </script>
 <style lang="scss" scroped>
 .webgl-container {
-  width: 800px;
-  height: 600px;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
